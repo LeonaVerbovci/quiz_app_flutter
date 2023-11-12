bb1300fbc758a405b1038a4a4f0b7081a44b1fe5
LeonaVerbovci <lonaverbovci@gmail.com>
2023-11-12 23:29:45 +0100

    Managed the question index

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,9 +12,19 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  answerQuestion() {
+    // currentQuestionIndex = currentQuestionIndex + 1;
+    //currentQuestionIndex += 1;
+    setState(() {
+      currentQuestionIndex++; //increments the value by 1
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
@@ -31,9 +41,12 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               ),
               textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 30),
+            const SizedBox(height: 40),
             ...currentQuestion.getShuffledAnswers().map((answer) {
-              return AnswerButton(answerText: answer, onTap: () {});
+              return AnswerButton(
+                answerText: answer,
+                onTap: answerQuestion,
+              );
             }),
           ],
         ),
